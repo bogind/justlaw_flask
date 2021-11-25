4d22576bc52e6295efe56983e8f9bed3f38ab5a5
bogind <dror.bogin@gmail.com>
2021-11-25 19:50:24 +0200

    added page for prisat_hov and income_assurance

--- server.py
+++ server.py
@@ -5,9 +5,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
     
+
+@app.route('/income_assurance/')
+def income_assurance():
+    return render_template('income_assurance.html')   
+
+
+@app.route('/prisat_hov/')
+def prisat_hov():
+    return render_template('prisat_hov.html')       
 
 if __name__ == '__main__':
     app.run( debug = True)
